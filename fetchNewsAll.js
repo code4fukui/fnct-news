2260613a75f7ef1fddacbb9652bff3a1b2bacdd5
Taisuke Fukuno <fukuno@jig.jp>
2021-09-11 08:07:27 +0900

    make csv file if not exist

--- fetchNewsAll.js
+++ fetchNewsAll.js
@@ -2,25 +2,39 @@ import { CSV } from "https://js.sabae.cc/CSV.js";
 import { fetchNews } from "./fetchNews.js";
 import { sleep } from "https://js.sabae.cc/sleep.js";
 
-const all = CSV.toJSON(await CSV.fetch("fnct-news.csv"));
+const all = await (async () => {
+  try {
+    return CSV.toJSON(await CSV.fetch("fnct-news.csv"));
+  } catch (e) {
+    console.log("not found file");
+    return [];
+  }
+})();
 
 const add = (data) => {
+  let nadd = 0;
   data.forEach(d => {
     const chk = all.find(a => a.url == d.url && a.title == d.title && a.date == d.date);
     if (!chk) {
       all.push(d);
+      nadd++;
     }
   });
+  return nadd;
 };
-add(await fetchNews("https://www.fukui-nct.ac.jp/news/"));
-for (let i = 2; i < 50; i++) {
-  console.log(i);
-  const res = await fetchNews("https://www.fukui-nct.ac.jp/news/page/" + i + "/");
-  if (res.length == 0) {
-    break;
+const nadd = add(await fetchNews("https://www.fukui-nct.ac.jp/news/"));
+if (nadd) {
+  for (let i = 2;; i++) {
+    console.log(i);
+    const res = await fetchNews("https://www.fukui-nct.ac.jp/news/page/" + i + "/");
+    if (res.length == 0) {
+      break;
+    }
+    await sleep(500);
+    if (!add(res)) {
+      break;
+    }
   }
-  await sleep(500);
-  add(res);
 }
 all.sort((a, b) => a.date.localeCompare(b.date));
 await Deno.writeTextFile("fnct-news.csv", CSV.stringify(all));
